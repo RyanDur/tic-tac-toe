0d6d3da544350325ea71d41cf9ee1a8226711ceb
RyanDur <ryan.p.durling@gmail.com>
2014-10-18 02:19:39 +0100

    should be able to set a pice on the board

--- src/main/java/views/Board.java
+++ src/main/java/views/Board.java
@@ -1,7 +1,9 @@
 package views;
 
 import controllers.GameCtrl;
-import javafx.geometry.Pos;
+import exceptions.NotVacantException;
+import exceptions.OutOfBoundsException;
+import exceptions.OutOfTurnException;
 import javafx.scene.Parent;
 import javafx.scene.control.Label;
 import javafx.scene.control.Labeled;
@@ -15,33 +17,66 @@ public class Board extends Parent {
     private final GameCtrl gameCtrl;
     private final GridPane grid;
     private final Label label;
+    private final Player player1;
+    private final Player player2;
+    private Player currentPlayer;
 
-    public Board(GameCtrl gameCtrl) {
+    public Board(GameCtrl gameCtrl, Player player1, Player player2) {
+        this.player1 = player1;
+        this.player2 = player2;
         this.getStylesheets().add(constants.BOARD_STYLE_SHEET);
         this.gameCtrl = gameCtrl;
         grid = new GridPane();
         grid.setId(constants.BOARD_ID);
-        grid.setAlignment(Pos.CENTER);
-        grid.setGridLinesVisible(true);
         label = new Label();
         GridPane.setConstraints(label, constants.SIDE, constants.SIDE);
-        grid.setPrefSize(600, 600);
         fillBoard(gameCtrl.getBoard());
         this.getChildren().add(grid);
     }
 
     private void fillBoard(Player[] board) {
-        System.out.println(board);
         for (int y = 0; y < constants.SIDE; y++) {
             for (int x = 0; x < constants.SIDE; x++) {
                 Labeled space = new Label();
-                //space.setAlignment(Pos.CENTER);
-                int position = (x * constants.SIDE) + y;
-
-                space.setText(" ");
+                String id = x + "," + y;
+                space.setId(id);
+                final int finalY = y;
+                final int finalX = x;
+                Player player = board[(x*constants.SIDE)+y];
+                if(player == null) {
+                    space.setOnMouseClicked(mouseEvent -> {
+                        try {
+                            setPiece(finalX, finalY);
+                        } catch (OutOfBoundsException e) {
+                            e.printStackTrace();
+                        }
+                    });
+                } else {
+                    space.setText(player.getPiece());
+                }
 
                 grid.add(space, x, y);
             }
         }
+    }
+
+    private void setPiece(int x, int y) throws OutOfBoundsException {
+        try {
+            Player player = getCurrentPlayer();
+            player.setCoordinates(x, y);
+            gameCtrl.setPiece(player);
+            fillBoard(gameCtrl.getBoard());
+        } catch (OutOfTurnException | NotVacantException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private Player getCurrentPlayer() {
+        if (currentPlayer == player1) {
+            currentPlayer = player2;
+        } else {
+            currentPlayer = player1;
+        }
+        return currentPlayer;
     }
 }
--- src/test/java/views/BoardTest.java
+++ src/test/java/views/BoardTest.java
@@ -1,6 +1,8 @@
 package views;
 
 import controllers.GameCtrl;
+import exceptions.NotVacantException;
+import exceptions.OutOfTurnException;
 import javafx.scene.Parent;
 import lang.constants;
 import models.Player;
@@ -9,23 +11,45 @@ import org.loadui.testfx.GuiTest;
 
 import static org.loadui.testfx.Assertions.verifyThat;
 import static org.loadui.testfx.controls.impl.ContainsNodesMatcher.contains;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 public class BoardTest extends GuiTest {
 
     private GameCtrl mockGameCtrl;
+    private Player[] board;
+    private Player mockPlayer1;
+    private Player mockPlayer2;
 
     @Override
     protected Parent getRootNode() {
+        mockPlayer1 = mock(Player.class);
+        mockPlayer2 = mock(Player.class);
         mockGameCtrl = mock(GameCtrl.class);
-        return new Board(mockGameCtrl);
+        board = new Player[constants.SIDE * constants.SIDE];
+        when(mockGameCtrl.getBoard()).thenReturn(board);
+        return new Board(mockGameCtrl, mockPlayer1, mockPlayer2);
     }
 
     @Test
     public void shouldHaveABoardThatReflectsTheBoardForTheGame() {
-        Player[] board = new Player[constants.SIDE * constants.SIDE];
-        when(mockGameCtrl.getBoard()).thenReturn(board);
         verifyThat("#board", contains(board.length, ".label"));
+    }
+
+    @Test
+    public void shouldBeAbleToChooseAPlaceOnTheBoard() throws OutOfTurnException, NotVacantException {
+        int x = 1;
+        int y = 1;
+        String piece = "X";
+        String id = "#" + x + "," + y;
+        board[calc(x, y)] = mockPlayer1;
+        when(mockPlayer1.getPiece()).thenReturn(piece);
+        when(mockGameCtrl.getBoard()).thenReturn(board);
+
+        click(id);
+        verify(mockGameCtrl).setPiece(any(Player.class));
+    }
+
+    private int calc(int x, int y) {
+        return (x * constants.SIDE) + y;
     }
 }
