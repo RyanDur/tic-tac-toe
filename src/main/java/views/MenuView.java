0b9bb3b528199dfdd54107663aa7415eb646c4df
RyanDur <ryan.p.durling@gmail.com>
2014-10-23 00:49:58 +0100

    player creation for when clicked

--- src/main/java/views/MenuView.java
+++ src/main/java/views/MenuView.java
@@ -1,30 +1,54 @@
 package views;
 
 import factories.PlayerFactory;
+import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.control.Button;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.BorderPane;
 import lang.constants;
 import models.Player;
 
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
 
 public class MenuView extends Parent {
 
     private PlayerFactory playerFactory;
+    private final Button buttonOne;
+    private final Button buttonTwo;
 
     public MenuView(PlayerFactory playerFactory) throws IOException {
         this.playerFactory = playerFactory;
-        List<Player> players = new ArrayList<>();
         BorderPane menu = FXMLLoader.load(getClass().getResource(constants.MENU_VIEW));
-        Button twoPlayer = (Button) menu.getRight();
-        twoPlayer.setOnMouseClicked(event -> {
-            players.add(playerFactory.createPlayer(constants.GAME_PIECE_ONE, constants.SIDE));
-            players.add(playerFactory.createPlayer(constants.GAME_PIECE_TWO, constants.SIDE));
-        });
+        buttonOne = (Button) menu.getLeft();
+        buttonTwo = (Button) menu.getRight();
+        buttonTwo.setOnMouseClicked(twoPlayer());
+        buttonOne.setOnMouseClicked(onePlayer());
         this.getChildren().add(menu);
+    }
+
+    private EventHandler<MouseEvent> onePlayer() {
+        return event -> {
+            buttonOne.setText(constants.GAME_PIECE_ONE);
+            buttonTwo.setText(constants.GAME_PIECE_TWO);
+
+            buttonOne.setOnMouseClicked(setOnePlayers(constants.GAME_PIECE_ONE, constants.GAME_PIECE_TWO));
+            buttonTwo.setOnMouseClicked(setOnePlayers(constants.GAME_PIECE_TWO, constants.GAME_PIECE_ONE));
+        };
+    }
+
+    private EventHandler<MouseEvent> setOnePlayers(String gamePiece, String gamePiece1) {
+        return event -> {
+            Player player = playerFactory.createPlayer(gamePiece, constants.SIDE);
+            playerFactory.createComputerPlayer(gamePiece1, constants.SIDE, player);
+        };
+    }
+
+    private EventHandler<MouseEvent> twoPlayer() {
+        return event -> {
+            playerFactory.createPlayer(constants.GAME_PIECE_ONE, constants.SIDE);
+            playerFactory.createPlayer(constants.GAME_PIECE_TWO, constants.SIDE);
+        };
     }
 }
--- src/test/java/views/MenuViewTest.java
+++ src/test/java/views/MenuViewTest.java
@@ -2,6 +2,8 @@ package views;
 
 import factories.PlayerFactory;
 import javafx.scene.Parent;
+import lang.constants;
+import models.Player;
 import org.junit.Test;
 import org.loadui.testfx.GuiTest;
 
@@ -11,14 +13,13 @@ import static org.loadui.testfx.Assertions.verifyThat;
 import static org.loadui.testfx.controls.Commons.hasText;
 import static org.mockito.Matchers.anyInt;
 import static org.mockito.Matchers.anyString;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.*;
 
 public class MenuViewTest extends GuiTest{
 
-    private final String twoPlayer = "#two_player";
+    private final String twoPlayerId = "#two_player";
     private final String onePlayer = "1 Player";
+    private final String onePlayerId = "#one_player";
     private PlayerFactory playerFactory;
 
     @Override
@@ -34,17 +35,46 @@ public class MenuViewTest extends GuiTest{
 
     @Test
     public void shouldHaveAOnePlayerButton() {
-        verifyThat("#one_player", hasText(onePlayer));
+        verifyThat(onePlayerId, hasText(onePlayer));
     }
 
     @Test
     public void shouldHaveATwoPlayerButton() {
-        verifyThat(twoPlayer, hasText("2 Player"));
+        verifyThat(twoPlayerId, hasText("2 Player"));
     }
 
     @Test
     public void shouldCreateTwoPlayersIfTwoPlayerIsChosen() {
-        click(twoPlayer);
+        click(twoPlayerId);
         verify(playerFactory, times(2)).createPlayer(anyString(), anyInt());
+    }
+
+    @Test
+    public void shouldChangeTheButtonsToChooseXOrOIfOnePlayerIsChosen() {
+        click(onePlayer);
+        verifyThat(onePlayerId, hasText(constants.GAME_PIECE_ONE));
+        verifyThat(twoPlayerId, hasText(constants.GAME_PIECE_TWO));
+    }
+
+    @Test
+    public void shouldMakeTheComputerPlayerXWhenOIsChosen() {
+        click(onePlayer);
+        click(constants.GAME_PIECE_TWO);
+
+        verify(playerFactory).createPlayer(constants.GAME_PIECE_TWO, constants.SIDE);
+        verify(playerFactory).createComputerPlayer(anyString(), anyInt(), any(Player.class));
+        verify(playerFactory).createComputerPlayer(constants.GAME_PIECE_ONE, constants.SIDE, null);
+    }
+
+    @Test
+    public void shouldMakeTheComputerPlayerOWhenXIsChosen() {
+        click(onePlayer);
+        click(constants.GAME_PIECE_ONE);
+        Player human = mock(Player.class);
+        when(playerFactory.createPlayer(anyString(),anyInt())).thenReturn(human);
+
+        verify(playerFactory).createPlayer(constants.GAME_PIECE_ONE, constants.SIDE);
+        verify(playerFactory).createComputerPlayer(anyString(), anyInt(), any(Player.class));
+        verify(playerFactory).createComputerPlayer(constants.GAME_PIECE_TWO, constants.SIDE, null);
     }
 }
