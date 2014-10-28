522f41839390ecc03f3712ad15e43f3f34b52c3c
RyanDur <ryan.p.durling@gmail.com>
2014-10-28 12:34:13 +0000

    remove public ststic final from declaration since it is an interface

--- src/main/java/lang/constants.java
+++ src/main/java/lang/constants.java
@@ -4,33 +4,33 @@ import java.util.Arrays;
 import java.util.List;
 
 public interface constants {
-    public static final String GAME_PIECE_ONE = "X";
-    public static final String GAME_PIECE_TWO = "O";
-    public static final int SIDE = 3;
-    public static final String GAME_VIEW = "/scenes/game.fxml";
-    public static final String MESSAGES_ID = "#messages";
-    public static final String DRAW_MESSAGE = "The Game is a Draw!";
-    public static final String NOT_VACANT_MESSAGE = "Space is occupied,\n please choose another.";
-    public static final String OUT_OF_BOUNDS_MESSAGE = "You chose a space out of bounds, HOW!!??";
-    public static final String OUT_OF_TURN_MESSAGE = "It's not your turn!";
-    public static final String EMPTY = "";
-    public static final List<Integer[]> CORNERS = Arrays.asList(
+    String GAME_PIECE_ONE = "X";
+    String GAME_PIECE_TWO = "O";
+    int SIDE = 3;
+    String GAME_VIEW = "/scenes/game.fxml";
+    String MESSAGES_ID = "#messages";
+    String DRAW_MESSAGE = "The Game is a Draw!";
+    String NOT_VACANT_MESSAGE = "Space is occupied,\n please choose another.";
+    String OUT_OF_BOUNDS_MESSAGE = "You chose a space out of bounds, HOW!!??";
+    String EMPTY = "";
+    String OUT_OF_TURN_MESSAGE = "It's not your turn!";
+    List<Integer[]> CORNERS = Arrays.asList(
             new Integer[]{0, 0},
             new Integer[]{0, SIDE - 1},
             new Integer[]{SIDE - 1, 0},
             new Integer[]{SIDE - 1, SIDE - 1});
-    public static final int WIN_WEIGHT = 1;
-    public static final int LOSE_WEIGHT = -1;
-    public static final int DRAW_WEIGHT = 0;
-    public static final String TIC_TAC_TOE_VIEW = "/scenes/tictactoe.fxml";
-    public static final String REPLAY_ID = "#replay";
-    public static final String RESET_ID = "#reset";
-    public static final String ONE_PLAYER = "1 Player";
-    public static final String TWO_PLAYER = "2 Player";
-    public static final Integer[] CENTER = new Integer[]{1,1};
-    public static final String HAS_WON_MESSAGE = " Has Won!!";
-    public static final String HEADER_VIEW = "/scenes/header.fxml";
-    public static final String RIGHT_BUTTON_ID = "#right";
-    public static final String LEFT_BUTTON_ID = "#left";
-    public static final String MENU_VIEW = "/scenes/menu.fxml";
+    int WIN_WEIGHT = 1;
+    int LOSE_WEIGHT = -1;
+    int DRAW_WEIGHT = 0;
+    String TIC_TAC_TOE_VIEW = "/scenes/tictactoe.fxml";
+    String REPLAY_ID = "#replay";
+    String RESET_ID = "#reset";
+    String ONE_PLAYER = "1 Player";
+    String TWO_PLAYER = "2 Player";
+    Integer[] CENTER = new Integer[]{1,1};
+    String HAS_WON_MESSAGE = " Has Won!!";
+    String HEADER_VIEW = "/scenes/header.fxml";
+    String RIGHT_BUTTON_ID = "#right";
+    String LEFT_BUTTON_ID = "#left";
+    String MENU_VIEW = "/scenes/menu.fxml";
 }
