84f32ec7dc2aa92b84b923f1bcde6ccf45b17db6
RyanDur <ryan.p.durling@gmail.com>
2014-11-01 18:08:14 +0000

    mved ai into player

--- src/main/java/modules/GameModule.java
+++ src/main/java/modules/GameModule.java
@@ -9,7 +9,6 @@ import tictactoe.ComputerPlayerImpl;
 public class GameModule extends AbstractModule {
     @Override
     protected void configure() {
-        install(new ComputerPlayerModule());
         bind(Board.class).to(BoardImpl.class);
         bind(ComputerPlayer.class).to(ComputerPlayerImpl.class);
     }
