f9c2b61323046540de659269f53bd8ad89be0f9c
RyanDur <ryan.p.durling@gmail.com>
2014-11-04 04:33:16 +0000

    refactor

--- src/main/java/tictactoe/ComputerPlayerImpl.java
+++ src/main/java/tictactoe/ComputerPlayerImpl.java
@@ -58,8 +58,8 @@ public class ComputerPlayerImpl implements ComputerPlayer {
      * based on the ending state of the game. If the game is not over, the available moves are gathered
      * to recursively build a game tree by playing each move and calling the containing method switching
      * pieces, to emulate game play, until the game is over creating the branches of the tree. Upon the
-     * completion of a branch, the min or max value is calculated depending on the piece that
-     * entered into the method last.
+     * completion of a branch, the min or max value is calculated depending on the piece that entered
+     * into the method last.
      *
      * @param piece of the last player
      * @param board that was just played on
@@ -67,9 +67,10 @@ public class ComputerPlayerImpl implements ComputerPlayer {
      */
     private int getScore(Character piece, Board board) {
         if (board.gameOver()) return score(board);
-        IntStream scores = getMoves(getOpponent(piece), board).stream()
-                .map(move -> playMove(getOpponent(piece), move, board))
-                .mapToInt(childBoard -> getScore(getOpponent(piece), childBoard));
+        Character opponent = getOpponent(piece);
+        IntStream scores = getMoves(opponent, board).stream()
+                .map(move -> playMove(opponent, move, board))
+                .mapToInt(childBoard -> getScore(opponent, childBoard));
         return getPiece().equals(piece) ? scores.min().getAsInt() : scores.max().getAsInt();
     }
 
@@ -89,39 +90,18 @@ public class ComputerPlayerImpl implements ComputerPlayer {
         return moves;
     }
 
-    /**
-     * If there is no winner then return the score for a draw. If there is a winner and it is the computer
-     * player return a wining score, else it is not so return a losing score.
-     *
-     * @param board that was played on
-     * @return the score
-     */
     private int score(Board board) {
         Character winner = board.getWinner();
         if (winner == null) return constants.DRAW_SCORE;
         return (winner.equals(getPiece())) ? constants.WIN_SCORE : constants.LOSE_SCORE;
     }
 
-    /**
-     * Filter out all the winning moves on the current board.
-     *
-     * @param piece representing the player
-     * @param board that was played on
-     * @return the moves that will gain a win.
-     */
     private Set<List<Integer>> findWinningMoves(Character piece, Board board) {
         return board.getVacancies().stream()
                 .filter(move -> playMove(piece, move, board).getWinner() != null)
                 .collect(toSet());
     }
 
-    /**
-     * Filter out all the losing moves on the current board.
-     *
-     * @param piece representing the player
-     * @param board that was played on
-     * @return the moves that will incur a loss.
-     */
     private Set<List<Integer>> findLosingMoves(Character piece, Board board) {
         return board.getVacancies().stream()
                 .map(move -> playMove(piece, move, board))
@@ -129,14 +109,6 @@ public class ComputerPlayerImpl implements ComputerPlayer {
                 .collect(toSet());
     }
 
-    /**
-     * Creates a copy of the board with the move attributed to the piece that was passed
-     *
-     * @param piece representing the player
-     * @param move for the board
-     * @param board to play on
-     * @return board copy with the move set upon it
-     */
     private Board playMove(Character piece, List<Integer> move, Board board) {
         board = board.copy();
         try {
