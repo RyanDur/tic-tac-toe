3032a4f609e0fde01829efad6f0380250c7312e9
RyanDur <ryan.p.durling@gmail.com>
2014-11-04 23:05:26 +0000

    change passing of piece to bolean

--- src/main/java/tictactoe/game/ComputerPlayerImpl.java
+++ src/main/java/tictactoe/game/ComputerPlayerImpl.java
@@ -16,6 +16,7 @@ import static java.util.stream.Collectors.toSet;
 public class ComputerPlayerImpl implements ComputerPlayer {
     private Character piece;
     private final Random random;
+    private Character opponent;
 
     public ComputerPlayerImpl() {
         random = new Random();
@@ -24,6 +25,7 @@ public class ComputerPlayerImpl implements ComputerPlayer {
     @Override
     public void setPiece(Character piece) {
         this.piece = piece;
+        setOpponent(piece);
     }
 
     @Override
@@ -40,50 +42,48 @@ public class ComputerPlayerImpl implements ComputerPlayer {
      * is just as good as another. Once the move is chosen, it is played on the board. Then the
      * board is returned.
      *
-     * @param board that represents the current state of the tictactoe.game
+     * @param board that represents the current state of the game
      * @return board that has the move of the computerPlayer placed on it.
      */
     @Override
     public Board calculateBestMove(Board board) {
-        Character piece = getPiece();
-        List<List<Integer>> maxMoves = getMoves(piece, board).parallelStream().collect(
-                groupingBy(move -> getScore(piece, playMove(piece, move, board)))).entrySet().stream()
+        List<List<Integer>> maxMoves = getMoves(true, board).parallelStream().collect(
+                groupingBy(move -> getScore(true, playMove(true, move, board)))).entrySet().stream()
                 .max((score1, score2) -> score1.getKey() - score2.getKey()).get().getValue();
         List<Integer> move = maxMoves.get(random.nextInt(maxMoves.size()));
-        return playMove(piece, move, board);
+        return playMove(true, move, board);
     }
 
     /**
-     * Upon entering the method, a check to see if the tictactoe.game is over. If it is a score is returned
-     * based on the ending state of the tictactoe.game. If the tictactoe.game is not over, the available moves are gathered
-     * to recursively build a tictactoe.game tree by playing each move and calling the containing method switching
-     * pieces, to emulate tictactoe.game play, until the tictactoe.game is over creating the branches of the tree. Upon the
+     * Upon entering the method, a check to see if the game is over. If it is a score is returned
+     * based on the ending state of the game. If the game is not over, the available moves are gathered
+     * to recursively build a game tree by playing each move and calling the containing method switching
+     * pieces, to emulate game play, until the game is over creating the branches of the tree. Upon the
      * completion of a branch, the min or max value is calculated depending on the piece that entered
      * into the method last.
      *
-     * @param piece of the last player
+     * @param isComputer true if the player is computer
      * @param board that was just played on
      * @return score of the turn
      */
-    private int getScore(Character piece, Board board) {
+    private int getScore(boolean isComputer, Board board) {
         if (board.gameOver()) return score(board);
-        Character opponent = getOpponent(piece);
-        IntStream scores = getMoves(opponent, board).stream()
-                .map(move -> playMove(opponent, move, board))
-                .mapToInt(childBoard -> getScore(opponent, childBoard));
-        return getPiece().equals(piece) ? scores.min().getAsInt() : scores.max().getAsInt();
+        IntStream scores = getMoves(!isComputer, board).stream()
+                .map(move -> playMove(!isComputer, move, board))
+                .mapToInt(childBoard -> getScore(!isComputer, childBoard));
+        return isComputer ? scores.min().getAsInt() : scores.max().getAsInt();
     }
 
     /**
      * To simulate the perfect player, the winning moves are first considered. If there are no moves
-     * that can win a tictactoe.game, then search for a move that will make player lose. If there are no losing
+     * that can win a game, then search for a move that will make player lose. If there are no losing
      * moves, then retrieve all the possible moves for consideration.
      *
-     * @param piece representing the player
+     * @param piece true if the player is computer
      * @param board that was played on
      * @return the moves to be used
      */
-    private Set<List<Integer>> getMoves(Character piece, Board board) {
+    private Set<List<Integer>> getMoves(boolean piece, Board board) {
         Set<List<Integer>> moves = findWinningMoves(piece, board);
         if (moves.isEmpty()) moves = findLosingMoves(piece, board);
         if (moves.isEmpty()) moves = board.getVacancies();
@@ -96,31 +96,35 @@ public class ComputerPlayerImpl implements ComputerPlayer {
         return (winner.equals(getPiece())) ? Constants.WIN_SCORE : Constants.LOSE_SCORE;
     }
 
-    private Set<List<Integer>> findWinningMoves(Character piece, Board board) {
+    private Set<List<Integer>> findWinningMoves(boolean computer, Board board) {
         return board.getVacancies().stream()
-                .filter(move -> playMove(piece, move, board).getWinner() != null)
+                .filter(move -> playMove(computer, move, board).getWinner() != null)
                 .collect(toSet());
     }
 
-    private Set<List<Integer>> findLosingMoves(Character piece, Board board) {
+    private Set<List<Integer>> findLosingMoves(boolean computer, Board board) {
         return board.getVacancies().stream()
-                .map(move -> playMove(piece, move, board))
-                .flatMap(childBoard -> findWinningMoves(getOpponent(piece), childBoard).stream())
+                .map(move -> playMove(computer, move, board))
+                .flatMap(childBoard -> findWinningMoves(!computer, childBoard).stream())
                 .collect(toSet());
     }
 
-    private Board playMove(Character piece, List<Integer> move, Board board) {
+    private Board playMove(boolean isComputer, List<Integer> move, Board board) {
         board = board.copy();
         try {
-            board.set(move.get(0), move.get(1), piece);
+            board.set(move.get(0), move.get(1), isComputer ? getPiece() : getOpponent());
         } catch (NotVacantException | OutOfBoundsException | OutOfTurnException e) {
             e.printStackTrace();
         }
         return board;
     }
 
-    private Character getOpponent(Character piece) {
-        return Constants.GAME_PIECE_ONE.equals(piece) ?
+    public Character getOpponent() {
+        return opponent;
+    }
+
+    private void setOpponent(Character piece) {
+        opponent = Constants.GAME_PIECE_ONE.equals(piece) ?
                 Constants.GAME_PIECE_TWO : Constants.GAME_PIECE_ONE;
     }
 }
