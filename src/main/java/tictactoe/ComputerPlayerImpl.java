6f3c62b731e4ef16a51b4bc173a502a44abc48de
RyanDur <ryan.p.durling@gmail.com>
2014-11-18 15:53:28 +0000

    static import constants

--- src/main/java/tictactoe/ComputerPlayerImpl.java
+++ src/main/java/tictactoe/ComputerPlayerImpl.java
@@ -1,7 +1,6 @@
 package tictactoe;
 
 import tictactoe.exceptions.InvalidMoveException;
-import tictactoe.lang.Constants;
 
 import java.util.LinkedList;
 import java.util.List;
@@ -10,6 +9,7 @@ import java.util.Set;
 import java.util.stream.Stream;
 
 import static java.util.stream.Collectors.*;
+import static tictactoe.lang.Constants.*;
 
 public class ComputerPlayerImpl implements ComputerPlayer {
     private Character piece;
@@ -27,8 +27,7 @@ public class ComputerPlayerImpl implements ComputerPlayer {
     @Override
     public List<Integer> getMove(Game game) {
         List<List<Integer>> maxMoves = getCandidates(game).parallelStream().collect(
-                groupingBy(move -> negaPruneDepth(Constants.POS_INF, Constants.NEG_INF,
-                        playMove(move, game), Constants.PLY, Constants.DEPTH))).entrySet().stream()
+                groupingBy(move -> negaPruneDepth(POS_INF, NEG_INF, play(move, game), PLY, DEPTH))).entrySet().stream()
                 .max((score1, score2) -> score1.getKey() - score2.getKey()).get().getValue();
         return maxMoves.get(random.nextInt(maxMoves.size()));
     }
@@ -36,32 +35,32 @@ public class ComputerPlayerImpl implements ComputerPlayer {
     private int negaPruneDepth(int alpha, int beta, Game game, int ply, int depth) {
         if (depth <= 0 || game.isOver()) return ply * score(game);
         LinkedList<Game> candidates = getCandidates(game).stream()
-                .map(move -> playMove(move, game)).collect(toCollection(LinkedList::new));
+                .map(move -> play(move, game)).collect(toCollection(LinkedList::new));
         while (alpha > beta && !candidates.isEmpty())
             alpha = Math.min(alpha, -negaPruneDepth(-beta, -alpha, candidates.pop(), -ply, depth - 1));
         return alpha;
     }
 
     private Set<List<Integer>> getCandidates(Game game) {
-        Set<List<Integer>> candidates = findWinningMoves(game, Constants.ROOT_PLY).collect(toSet());
-        if (candidates.isEmpty()) candidates = findWinningMoves(game, Constants.ROOT_PLY + 1).collect(toSet());
+        Set<List<Integer>> candidates = findWinMoves(game, ROOT_PLY).collect(toSet());
+        if (candidates.isEmpty()) candidates = findWinMoves(game, ROOT_PLY + 1).collect(toSet());
         return candidates.isEmpty() ? game.getVacancies() : candidates;
     }
 
     private int score(Game game) {
         Character winner = game.getWinner();
-        if (winner == null) return Constants.DRAW_SCORE;
-        int extraWins = (int) findWinningMoves(game, Constants.ROOT_PLY + 1).distinct().count();
-        return piece.equals(winner) ? Constants.WIN_SCORE + extraWins : Constants.LOSE_SCORE - extraWins;
+        if (winner == null) return DRAW_SCORE;
+        int extraWins = (int) findWinMoves(game, ROOT_PLY + 1).distinct().count();
+        return piece.equals(winner) ? WIN_SCORE + extraWins : LOSE_SCORE - extraWins;
     }
 
-    private Stream<List<Integer>> findWinningMoves(Game game, int depth) {
+    private Stream<List<Integer>> findWinMoves(Game game, int depth) {
         Stream<List<Integer>> candidates = game.getVacancies().stream();
-        if (depth <= 0) return candidates.filter(move -> playMove(move, game).getWinner() != null);
-        return candidates.map(move -> playMove(move, game)).flatMap(child -> findWinningMoves(child, depth - 1));
+        if (depth <= 0) return candidates.filter(move -> play(move, game).getWinner() != null);
+        return candidates.map(move -> play(move, game)).flatMap(child -> findWinMoves(child, depth - 1));
     }
 
-    private Game playMove(List<Integer> move, Game game) {
+    private Game play(List<Integer> move, Game game) {
         game = game.copy();
         try {
             game.set(move);
